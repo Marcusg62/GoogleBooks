0ae82d7e46997b640b8003670859bb654fe734a1
Jazmin Barraza <68868686+barrajaz@users.noreply.github.com>
2021-03-04 20:49:17 -0700

    final version

--- script.js
+++ script.js
@@ -10,23 +10,24 @@ function searchGoogleBooks()
 
     document.querySelector("#mainDiv").classList.remove("hasError")
 
-    fetch("https://www.googleapis.com/books/v1/volumes?&q=" + txtSearch.value + "&startIndex=0&maxResults=20")
+    fetch("https://www.googleapis.com/books/v1/volumes?q=" + txtSearch.value + "&startIndex=0&maxResults=20")
     .then(response => response.json())
     .then(json => {
         var list = `<div class="row col-sm-5">found ${json.totalItems} books</div>
         <div class="row">`
-        for(let i = 0; i < json.items[i].volumeInfo.title;i++)
+        for(let i = 0; i < json.items.length; i++)
         {
             console.log(json.items[i].volumeInfo.title)
             var bookInfo = json.items[i].volumeInfo;
             var template = `<ul class="col-sm-12 col-md-6 col-xl-4">`;
-            template += `<a class="list-group-item list-group-item-action" href="${json.items[i].selflink}">${bookInfo.title}</a>`
+            template += `<a class="list-group-item list-group-item-action active" href="${json.items[i].selflink}">${bookInfo.title}</a>`
             template += `<li class="list-group-item">${bookInfo.authors}</li>`
             template += `<li class="list-group-item">${bookInfo.publisher}, ${bookInfo.publishedDate}</li>`
             template += `</ul>`
             list += template;
         }
         list += `</div>`
+        console.log('list', list)
         document.getElementById("mainDiv").innerHTML = list;
         
     });
