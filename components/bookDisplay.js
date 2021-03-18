527269008f287bbd79a613de2e5ab137bf6e0a44
Marcus Gallegos <marcus.gallegos@ucdenver.edu>
2021-03-17 23:04:59 -0600

    polishing changes

--- components/bookDisplay.js
+++ components/bookDisplay.js
@@ -8,12 +8,12 @@ app.component('book-display', {
     },
     template:
         /*html*/
-        `<ul class="col-sm-12 col-md-6 col-xl-4">
-        <a class="list-group-item list-group-item-action active" :href="this.BookObj.selflink"> {{ this.BookObj.volumeInfo.title }} </a>
+        `<ul class="col-sm-12 col-md-6 col-xl-4 my-2">
+        <a class="list-group-item list-group-item-action active text-light cursor-pointer underline" style="cursor: pointer; text-decoration: underline; " :href="this.BookObj.volumeInfo.selflink"> {{ this.BookObj.volumeInfo.title }} </a>
         <img v-if="this.BookObj.volumeInfo.imageLinks?.thumbnail" :src="this.BookObj.volumeInfo.imageLinks.thumbnail" class= "card-img-top" alt="book pic" >
-        <li class="list-group-item" v-if="this.BookObj.volumeInfo.averageRating"> {{ this.BookObj.volumeInfo.averageRating }} </li>
-        <li class="list-group-item" v-if="this.BookObj.volumeInfo.description"> {{ this.BookObj.volumeInfo.description }} </li>
-        <li class="list-group-item" v-if="this.BookObj.volumeInfo.authors"><span v-for="(author, jndex) in this.BookObj.volumeInfo.authors"> {{ author }}, </span> </li>
+        <li class="list-group-item" v-if="this.BookObj.volumeInfo.averageRating">Avg. Rating: {{ this.BookObj.volumeInfo.averageRating }}/5 </li>
+        <li class="list-group-item" v-if="this.BookObj.volumeInfo.description">Description:  {{ this.BookObj.volumeInfo.description }} </li>
+        <li class="list-group-item" v-if="this.BookObj.volumeInfo.authors">Authors: <span v-for="(author, jndex) in this.BookObj.volumeInfo.authors"> {{ author }}, </span> </li>
         <li class="list-group-item" v-if="(this.BookObj.volumeInfo.publisher && this.BookObj.volumeInfo.publishedDate)">{{this.BookObj.volumeInfo.publisher}},{{this.BookObj.volumeInfo.publishedDate}}</li>
         </ul>`,
 
