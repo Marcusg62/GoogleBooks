b40b8217e88a7b877edfc039d43c449253406989
Jazmin Barraza <68868686+barrajaz@users.noreply.github.com>
2021-03-17 21:36:59 -0600

    functionality

--- components/bookDisplay.js
+++ components/bookDisplay.js
@@ -4,14 +4,17 @@ app.component('book-display', {
             type: String,
             required: true,
             default: null
-        }
+        },
     },
     template:
         /*html*/
         `<ul class="col-sm-12 col-md-6 col-xl-4">
         <a class="list-group-item list-group-item-action active" :href="this.BookObj.selflink"> {{ this.BookObj.volumeInfo.title }} </a>
-        <li class="list-group-item"> {{ this.BookObj.volumeInfo.authors }} </li>
-        <li class="list-group-item">{{this.BookObj.volumeInfo.publisher}},{{this.BookObj.volumeInfo.publishedDate}}</li>
+        <img v-if="this.BookObj.volumeInfo.imageLinks?.thumbnail" :src="this.BookObj.volumeInfo.imageLinks.thumbnail" class= "card-img-top" alt="book pic" >
+        <li class="list-group-item" v-if="this.BookObj.volumeInfo.averageRating"> {{ this.BookObj.volumeInfo.averageRating }} </li>
+        <li class="list-group-item" v-if="this.BookObj.volumeInfo.description"> {{ this.BookObj.volumeInfo.description }} </li>
+        <li class="list-group-item" v-if="this.BookObj.volumeInfo.authors"><span v-for="(author, jndex) in this.BookObj.volumeInfo.authors"> {{ author }}, </span> </li>
+        <li class="list-group-item" v-if="(this.BookObj.volumeInfo.publisher && this.BookObj.volumeInfo.publishedDate)">{{this.BookObj.volumeInfo.publisher}},{{this.BookObj.volumeInfo.publishedDate}}</li>
         </ul>`,
 
     computed:
@@ -23,5 +26,10 @@ app.component('book-display', {
                 return null;
         }
     }
+});
 
-})
+
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -2,18 +2,43 @@ const app = Vue.createApp({
     data()  {
         return{
                 keyword: '',
-                result: null
+                result: null,
+                PageNum: 0,
+                max: 20,
+                x: 0
         }
     },
     methods: {
         SearchGoogleBooks() {
-            fetch("https://www.googleapis.com/books/v1/volumes?q=" + this.keyword + "&startIndex=0&maxResults=20")
+            fetch("https://www.googleapis.com/books/v1/volumes?q=" + txtSearch.value + "&startIndex="+this.PageNum+"&maxResults="+this.max)
             .then(response => response.json())
             .then((json) => {this.result = json
                 console.log(this.result)
-            }) 
-            
-        }   
+                this.x=this.result.totalItems
+            })
+        },
+        prevPage(){
+            if(this.PageNum == 0)
+            {
+                return;
+            }
+            this.PageNum -= 1;
+            this.SearchGoogleBooks()
+        },
+        nextPage(){
+            console.log(this.x);
+            console.log(this.max);
+            if(this.PageNum == parseInt(this.x / this.max))
+            {
+                console.log(this.max);
+                return;
+            }
+            this.PageNum += 1;
+            this.SearchGoogleBooks()
+        }
     }
+});
 
-})
+
+
+
